7789a9b9110aa4a283c7132d3de5b3e62a138650
34626 <34626@DESKTOP-0I2127B>
2023-02-28 20:58:13 +0100

    Nuevo paquete y nueva clase

---
+++ PracticaGit/src/clasetres/Conserje.java
@@ -0,0 +1,20 @@
+package clasetres;
+
+public class Conserje {
+	
+	private String nombre;
+	private int edad;
+	public Conserje() {
+		super();
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	
+	
+	
+	
+}
